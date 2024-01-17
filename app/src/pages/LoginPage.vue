a031691dcec8516d1fe4f9295dfbbd2451f45f3d
Jo-han <jo-han@miloctav.fr>
2024-01-17 12:18:16 +0100

    services init + base routing

---
+++ app/src/pages/LoginPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <q-card class="loginCard q-pa-md">
+      <q-input filled v-model="username" label="Username" lazy-rules
+        :rules="[val => !!val || 'Please enter your username']"></q-input>
+      <q-input filled v-model="password" label="Password" lazy-rules
+        :rules="[val => !!val || 'Please enter your password']" type="password"></q-input>
+      <q-btn class="q-mt-md" color="primary" label="Login" />
+    </q-card>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LoginPage',
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.loginCard {
+  width: 400px;
+  margin: 0 auto;
+  margin-top: 100px;
+}
+</style>
--- app/src/router/routes.js
+++ app/src/router/routes.js
@@ -1,10 +1,18 @@
 
 const routes = [
+  // {
+  //   path: '/',
+  //   component: () => import('layouts/MainLayout.vue'),
+  // },
   {
     path: '/',
+    component: () => import('pages/LoginPage.vue'),
+  },
+  {
+    path: '/home',
     component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+    children: [ 
+      // { path: '', component: () => import('pages/IndexPage.vue') },
     ]
   },
 
